ad4c4530007629c7ced54f05e419a810718c0822
IzabelaND <izabela.ndias@gmail.com>
2022-11-15 23:50:36 -0300

    commit feito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjEahxVeHGzdFSjglrL6QRY_ng-DUuLtjR-Q&usqp=CAU" title="Beef Burger" calories="70 calories" price="$12.00" />
+      <Card img="https://static.onecms.io/wp-content/uploads/sites/43/2022/02/16/21014-Good-old-Fashioned-Pancakes-mfs_001.jpg" title="Pancakes" calories="60 calories" price="$15.00" />
     </div>
   );
+}
+
+function Card (props){
+  return(
+    
+    <div className="card">
+      <div class="container">
+       <ul>
+          <h1 className='card__title'>{props.title}</h1>
+          <div class="container_calories">
+            <img src='https://images.emojiterra.com/google/android-11/512px/1f525.png'alt='' class="calories__image"/>
+            <p className='card__calories'>{props.calories}</p>
+          </div>
+          <p className='card__price'>{props.price}</p>
+          <button className="card__btn">Saiba mais</button>
+        </ul>
+        <img src={props.img} alt='' class="card__image" />
+      </div>
+    </div>
+
+  )
 }
 
 export default App;
